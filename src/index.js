82babd77364538b3c13e7706c5ca96eecc865837
lttb <connect@lttb.net>
2017-01-09 19:24:43 +0300

    Fix error handling and pass store onmessage and onclose

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 export default (init, {
   prepareAction = action => JSON.stringify({ action }),
+
   isWSAT = action => action.wsat !== false,
+
   getAction = ({ data }) => {
     const { action } = JSON.parse(data);
 
@@ -9,36 +11,39 @@ export default (init, {
 } = {}) => {
   let socket = init();
 
-  const { onclose, onopen, onmessage } = socket;
+  const { onclose, onmessage } = socket;
 
   const ws = (store) => {
     socket.onmessage = (message) => {
       const action = getAction(message);
 
       action && store.dispatch(action);
-      onmessage && onmessage(message);
+
+      onmessage && onmessage({ store, message });
     };
 
-    return next => action => (isWSAT(action)
+    socket.onclose = () => {
+      socket = Object.assign(init(), {
+        onmessage: socket.onmessage,
+        onclose: socket.onclose,
+      });
+
+      onclose && onclose({ store });
+    };
+
+    return next => action => (socket.readyState === 1 && isWSAT(action)
       ? socket.send(prepareAction(action))
       : next(action)
     );
   };
 
-  socket.onclose = () => {
-    socket = Object.assign(init(), {
-      onmessage: socket.onmessage,
-      onclose: socket.onclose,
-    });
-
-    onclose && onclose();
-  };
-
-  return new Promise((resolve) => {
+  return new Promise((resolve, reject) => {
     socket.onopen = () => {
-      onopen();
-
       resolve({ ws, socket });
+    };
+
+    socket.onerror = (error) => {
+      reject({ error, ws, socket });
     };
   });
 };
